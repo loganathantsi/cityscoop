602e09520573bf3cd8168a407b8d971b4b32fee9
Sankaranarayanan <sankar@tendersoftware.in>
2025-03-04 12:36:08 +0530

    notification badge added

--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -1,14 +1,20 @@
 import 'package:CityScoop/UI/dashboard.dart';
+import 'package:badges/badges.dart' as badges;
 import 'package:CityScoop/UI/dialog_logout.dart';
 import 'package:CityScoop/UI/dialog_notifications.dart';
 import 'package:CityScoop/constants/strings.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavigation extends StatelessWidget {
-  const BottomNavigation({super.key});
+  BottomNavigation({super.key});
+
+  int notificationBadgeAmount = 1;
+  bool showNotificationBadge = true;
+  Color color = Colors.red;
 
   @override
   Widget build(BuildContext context) {
+    showNotificationBadge = notificationBadgeAmount > 0;
 
     return Container(
         padding: EdgeInsets.fromLTRB(5, 8, 5, 0),
@@ -23,7 +29,7 @@ class BottomNavigation extends StatelessWidget {
               },
               child: Column(
                 children: [
-                  SizedBox(height: 25, child: Image.asset(Strings.notificationIcon, alignment: Alignment.center)),
+                  notificationBadge(),
                   Text("NOTICES", style: TextStyle(color: Colors.grey), textAlign: TextAlign.center),
                 ],
               ),
@@ -66,6 +72,7 @@ class BottomNavigation extends StatelessWidget {
   void dialogNotifications(BuildContext context) {
     showDialog(
       context: context,
+      barrierDismissible: false,
       builder: (context) => DialogNotifications(),
     );
   }
@@ -73,7 +80,24 @@ class BottomNavigation extends StatelessWidget {
   void dialogLogout(BuildContext context) {
     showDialog(
       context: context,
+      barrierDismissible: false,
       builder: (context) => DialogLogout(),
+    );
+  }
+
+  Widget notificationBadge() {
+    return badges.Badge(
+      position: badges.BadgePosition.topEnd(top: -8, end: -12),
+      badgeAnimation: badges.BadgeAnimation.slide(),
+      showBadge: showNotificationBadge,
+      badgeStyle: badges.BadgeStyle(
+        badgeColor: color,
+      ),
+      badgeContent: Text(
+        notificationBadgeAmount.toString(),
+        style: TextStyle(color: Colors.white),
+      ),
+      child: SizedBox(height: 25, child: Image.asset(Strings.notificationIcon, alignment: Alignment.center)),
     );
   }
 
