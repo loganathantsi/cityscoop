53e4a510861fef70c304e5ee072aaeda6318700e
Sankaranarayanan <sankar@tendersoftware.in>
2025-03-27 11:15:04 +0530

    qa fix

--- lib/UI/login.dart
+++ lib/UI/login.dart
@@ -291,33 +291,30 @@ class LoginScreenState extends State<LoginScreen> with SingleTickerProviderState
     // Foreground notifications
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
 
-      print("---> Foreground Notification: ${message.notification?.title}");
-      print("---> Foreground Data: ${message.data['redirect_url']}");
+      print("---> Foreground Notification: message.notification?.title ${message.notification?.title}");
+      print("---> Foreground Notification: message.data['redirect_url'] ${message.data['redirect_url']}");
+      print("---> Foreground Notification: message.data['click_action'] ${message.data['click_action']}");
 
     });
 
     // When app is in background & user taps the notification
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-
       print("---> Notification (Background): message.notification?.title ${message.notification?.title}");
       print("---> Notification (Background): message.data['redirect_url'] ${message.data['redirect_url']}");
+      print("---> Notification (Background): message.data['click_action'] ${message.data['click_action']}");
 
-      setState(() {
-        openUrlInBrowser("${message.data['redirect_url']}");
-      });
+      openUrlInBrowser("${message.data['redirect_url']}");
 
     });
 
     // When app is terminated and opened via notification
     FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage? message) {
       if (message != null) {
-
         print("---> Notification (Terminated): message.notification?.title ${message.notification?.title}");
         print("---> Notification (Terminated): message.data['redirect_url'] ${message.data['redirect_url']}");
+        print("---> Notification (Terminated): message.data['click_action'] ${message.data['click_action']}");
 
-        setState(() {
-          openUrlInBrowser("${message.data['redirect_url']}");
-        });
+        openUrlInBrowser("${message.data['redirect_url']}");
 
       }
     });
